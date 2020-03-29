022d6c2ef12131d72d52f20f5003701804c35ef9
staniewzki <m.staniewzki@gmail.com>
2020-03-29 19:13:09 +0200

    Models changes

--- problem/models.py
+++ problem/models.py
@@ -10,12 +10,14 @@ class Problem(models.Model):
 
 class SolutionTag(models.Model):
     name = models.CharField(max_length=100)
+    problems = models.ManyToManyField(Problem)
 
     def __str__(self)
         return self.name
 
 class SourceTag(models.Model):
     name = models.CharField(max_length=100)
+    problems = models.ManyToManyField(Problem)
 
     def __str__(self)
         return self.name
