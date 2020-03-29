d359a2e1b5d4800464a50e3fd32c97c147f134c4
staniewzki <m.staniewzki@gmail.com>
2020-03-29 18:47:40 +0200

    Created models

--- folder/models.py
+++ folder/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Folder(models.Model):
+    parent_folder = Mmodels.ForeignKey('self', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
--- problem/models.py
+++ problem/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Problem(models.Model):
+    content = models.TextField()
+
+    def __str__(self):
+        return self.content[:20] + "..."
+
+class SolutionTag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self)
+        return self.name
+
+class SourceTag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self)
+        return self.name
