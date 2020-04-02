15004405eebc80561539633efd3bbc779df5f956
Tomasz Nowak <tomek.nowak.gm.16@gmail.com>
2020-04-02 20:13:38 +0200

    Added deletion of folders

--- folder/urls.py
+++ folder/urls.py
@@ -6,6 +6,9 @@ app_name = 'folder'
 urlpatterns = [
     path('<path:folder_path>/edit', views.EditView.as_view(), name='edit'),
     path('<path:folder_path>/add_folder', views.AddFolder.as_view(), name='add_folder'),
+    path('<path:folder_path>/edit_folder_name', views.EditFolderName.as_view(), name='edit_folder_name'),
+    path('<path:folder_path>/delete_folder', views.DeleteFolder.as_view(), name='delete_folder'),
     path('<path:folder_path>/add_problem', views.AddProblem.as_view(), name='add_problem'),
+    path('<path:folder_path>/delete_problem', views.DeleteProblem.as_view(), name='delete_problem'),
     path('<path:folder_path>', views.IndexView.as_view(), name='index'),
 ]
