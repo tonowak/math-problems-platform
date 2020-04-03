4cb03d4a86d3adfb161a85ac34848eb4d5547f2b
Tomasz Nowak <tomek.nowak.gm.16@gmail.com>
2020-04-03 11:43:19 +0200

    Fixed selectize UI

--- static/scripts.js
+++ static/scripts.js
@@ -1,8 +1,1 @@
-window.onload = function () {
-  if(window.location.href == localStorage.getItem('lastUrl')) {
-	mainScrollArea.scrollTop = localStorage.getItem('scrollTop');
-  } else {
-	localStorage.setItem('lastUrl', window.location.href);
-	localStorage.setItem('scrollTop', 0);
-  }
-}
+// empty :O
