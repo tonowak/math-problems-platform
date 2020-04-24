5b664a75417d3211f686b43316e21052ea5b70b9
Tomasz Nowak <tomek.nowak.gm.16@gmail.com>
2020-04-24 17:27:20 +0200

    Added markdown

---
+++ tags/templatetags/markdown.py
@@ -0,0 +1,20 @@
+from django import template
+from markdown import markdown
+
+register = template.Library()
+
+@register.tag(name="markdown")
+def markdown_tag(parser, token):
+    nodelist = parser.parse(('endmarkdown',))
+    parser.delete_first_token()
+    return MarkdownNode(nodelist)
+
+class MarkdownNode(template.Node):
+    def __init__(self, nodelist):
+        self.nodelist = nodelist
+    
+    def render(self, context):
+        output = self.nodelist.render(context)
+        output = markdown(output)
+        output = '<span class="markdown">' + output + '</span>'
+        return output
